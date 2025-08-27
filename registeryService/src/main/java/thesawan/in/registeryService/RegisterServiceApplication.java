e01216f82b19dee7f6c1278d5caba339cda28086
Sawan Kumar <sawankushwaha222@gmail.com>
2025-08-27 23:16:25 +0530

    Register Service Done

---
+++ registeryService/src/main/java/thesawan/in/registeryService/RegisterServiceApplication.java
@@ -0,0 +1,16 @@
+package thesawan.in.registeryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+                RegisterServiceApplication.class, args);
+	}
+
+}
---
+++ registeryService/src/test/java/thesawan/in/registeryService/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package thesawan.in.registeryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
